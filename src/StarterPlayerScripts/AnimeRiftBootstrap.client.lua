b247f81a3ef518cc636b9fa1c6f903f20e07eb65
Dev0MostZaraki <mostwant3d39@gmail.com>
2026-09-14 20:02:08 +0200

    Remove temporary client sync bootstrap

--- src/StarterPlayerScripts/AnimeRiftBootstrap.client.lua
+++
@@ -1,5 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local Version = require(ReplicatedStorage:WaitForChild("AnimeRift"):WaitForChild("Version"))
-
-print(string.format("[%s] client sync active - %s", Version.Name, Version.Version))
